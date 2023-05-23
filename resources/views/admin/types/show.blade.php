366d12d4e1b082d76b8791006fa7bb4b38bcb076
MarcoCocco <marcococco28@gmail.com>
2023-05-23 18:21:20 +0200

    feat: aggiunta una condizione: se non ci sono tipi associati a progetti appare un messaggio personalizzato

--- resources/views/admin/types/show.blade.php
+++ resources/views/admin/types/show.blade.php
@@ -2,6 +2,7 @@
 
 @section('content')
     <h2 class="text-center m-4">Tutti i progetti di tipo {{ $type->name }} </h2>
+    @if (count($type->projects) > 0)
     <table class="mt-5 table table-striped">
         <thead>
             <th>Nome</th>
@@ -18,4 +19,7 @@
             @endforeach
         </tbody>
     </table>
+    @else 
+    <p class="text-center p-4">Il tipo {{ $type->name }} non ha nessun progetto associato.</p>
+    @endif
 @endsection
